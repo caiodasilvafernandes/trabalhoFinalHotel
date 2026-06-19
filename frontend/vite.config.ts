fb86b96bc26e25ccff6215facc6cc48360d53596
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-06-19 20:39:55 -0300

    chore(frontend): apply formatter fixes to config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import path from "path"
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-     tanstackRouter({
-      target: 'react',
+    tanstackRouter({
+      target: "react",
       autoCodeSplitting: true,
-      generatedRouteTree: './src/route-tree.gen.ts',
-      routesDirectory: './src/pages',
-      routeToken: 'layout',
+      generatedRouteTree: "./src/route-tree.gen.ts",
+      routesDirectory: "./src/pages",
+      routeToken: "layout",
     }),
     react(),
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
   resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
-})
+  },
+});
